27fbbcda82f90a09fc6483ccb8013514f8798e45
rana <ranasobeid95@gmail.com>
2019-07-30 17:11:35 +0300

    use request module instead of http module Relates #7

--- src/controllers/request.js
+++ src/controllers/request.js
@@ -1,31 +1,21 @@
-const http = require('http');
+const request = require('request');
 const value ='cat';
 const gifUrl = `http://api.giphy.com/v1/gifs/search?q=${value}&limit=10&api_key=dc6zaTOxFJmzC`;
 
 
 const gifReqest = (url) => {
     const gifPromise = new Promise((resolve,reject)=>{
-    http.get(url, response =>{
-        let allData = '';
-        response.on('data', (chunk)=>{
-            allData += chunk;
+    request(url, (err, response ,body )=>{
+         if(err){
+            reject('Error : response is no JSON');
+        }
+            resolve(JSON.parse(body));
         });
-        response.on('end', ()=>{
-            try {
-                resolve(JSON.parse(allData));
-                
-            } catch (error) {
-                reject('Error : response is no JSON');
-            }
-        })
-        }).on('error', (error) => {
-            reject(error.message);
-        });
+        
     });
-
-    return gifPromise;
+   return gifPromise;
 };
 
 gifReqest(gifUrl)
-.then(res =>  res.data[0].images.fixed_width.webp )
+.then(res =>  console.log(res.data[0].images.fixed_width.webp ))
 .catch(error => console.log(error.message));
