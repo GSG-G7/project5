4f4cc902b07295c3efaa2c5f40f5d9ae5e0b7611
AlaaTaima <alaa.ktaima@gmail.com>
2019-07-31 14:15:02 +0300

    get data from backend Relates #7

--- public/request.js
+++ public/request.js
@@ -1,19 +1,15 @@
-
-// const fetch = require("node-fetch");
-
-console.log("Request.js")
-
 const url  = '/search';
 
 document.getElementById('search_form').addEventListener('submit',(e)=>{
     console.log("search form")
     e.preventDefault();
     const input = document.getElementById('search__input').value;
-    
         fetch(url, {
             method: 'POST',
             headers : {'Content-Type':'application/json'},
             body:JSON.stringify({input:input})
-        }).then((res) => res.json())
+        })
+        .then((res) => res.json())
+        .then((res) => console.log(res.data[0].images.fixed_width.webp))
         .catch((err)=>console.log(err))
-})
+}); 
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const request = require('./request')
-
-
 const error = require('./error');
+
 
 router.get('/search',request.get)
 router.post('/search',request.post)
